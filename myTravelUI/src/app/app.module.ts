b17a287d3da87df8f1af64d008c57868a1cfdde1
andreamoncayo13 <andrea.moncayo2013@gmail.com>
2023-04-18 21:34:20 -0400

    upates 4/16/23

--- myTravelUI/src/app/app.module.ts
+++ myTravelUI/src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { VlogComponent } from './components/vlog/vlog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     SignupComponent,
-    DashboardComponent
+    DashboardComponent,
+    VlogComponent
   ],
   imports: [
     BrowserModule,
---
+++ myTravelUI/src/app/components/vlog/vlog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vlog',
+  templateUrl: './vlog.component.html',
+  styleUrls: ['./vlog.component.scss']
+})
+export class VlogComponent {
+
+}
